6c3f824ab2f350a2dfa6724b78ccc9f342b6ef4a
Isaiah Leiva <isaiah.leiva@gmail.com>
2022-02-07 16:02:41 -0700

    commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let blazersScore = 0
+let lakersScore = 0
+
+function increaseBlazersScore() {
+    blazersScore++
+    console.log('blazer score', blazersScore)
+}
+
+function increaseLakersScore() {
+    lakersScore++
+    console.log('laker function', lakersScore)
+}
